8333c66562040014eeac5949348bc8e403d297b5
[Taufiq Dhanial] <[taufiq.dhanil@gmail.com]>
2023-01-11 00:54:03 +0700

    user login

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,7 +1,8 @@
 import { json } from "@remix-run/node";
 import { prisma } from "./prisma.server";
-import { RegisterForm } from "./types.server";
+import { LoginForm, RegisterForm } from "./types.server";
 import { createUser } from "./user.server";
+import bcrypt from "bcryptjs";
 
 export async function register(user: RegisterForm) {
   const exist = await prisma.user.count({ where: { email: user.email } });
@@ -22,4 +23,11 @@ export async function register(user: RegisterForm) {
       { status: 400 }
     );
   }
+}
+
+export async function login({ email, password }: LoginForm) {
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user || !(await bcrypt.compare(password, user.password)))
+    return json({ error: `Incorrect login` }, { status: 400 });
+  return { id: user.id, email };
 }
