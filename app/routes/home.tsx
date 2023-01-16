1df414294acade0b692bbd692217840fff4ca396
[Taufiq Dhanial] <[taufiq.dhanil@gmail.com]>
2023-01-17 02:13:30 +0700

    build search bar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -7,6 +7,7 @@ import { getFilteredKudos } from "~/utils/kudo.server";
 import { getOtherUsers } from "~/utils/user.server";
 import { Kudo as IKudo, Profile } from "@prisma/client";
 import { Kudo } from "~/components/Kudo";
+import { SearchBar } from "~/components/SearchBar";
 
 interface kudoWithProfile extends IKudo {
   author: {
@@ -29,7 +30,7 @@ export default function Home() {
       <div className="h-full flex">
         <UserPanel users={users} />
         <div className="flex-1 flex flex-col">
-          {/* Search Bar Goes Here */}
+          <SearchBar />
           <div className="flex-1 flex">
             <div className="w-full p-10 flex flex-col gap-y-4">
               {kudos.map((kudo: kudoWithProfile) => (
--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -19,3 +19,18 @@ export const emojiMap = {
   PARTY: "🎉",
   HANDSUP: "🙌",
 };
+
+export const sortOptions = [
+  {
+    name: "Date",
+    value: "date",
+  },
+  {
+    name: "Sender Name",
+    value: "sender",
+  },
+  {
+    name: "Emoji",
+    value: "emoji",
+  },
+];
