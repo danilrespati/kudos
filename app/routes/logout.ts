e909a24fb467d6c2e5b699a1eb7b95a9683e611f
[Taufiq Dhanial] <[taufiq.dhanil@gmail.com]>
2023-01-19 01:49:36 +0700

    code refactoring

--- app/routes/logout.ts
+++ app/routes/logout.ts
@@ -2,5 +2,10 @@ import { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { redirect } from "react-router";
 import { logout } from "~/utils/auth.server";
 
-export const loader: LoaderFunction = async () => redirect("/");
-export const action: ActionFunction = async ({ request }) => logout(request);
+export const loader: LoaderFunction = async () => {
+  return redirect("/");
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  return logout(request);
+};
